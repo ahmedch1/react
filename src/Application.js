be9d28203d3bb45ead7ccb362913b2d21050cf0d
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-19 11:14:35 +0100

    react lifecycle

--- src/Application.js
+++ src/Application.js
@@ -1,6 +1,26 @@
 import React, {Component} from "react";
 
 class Application extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount(props, state) {
+    }
+
+    componentDidMount(props, state) {
+        console.log("Mounted with", props, state);
+    }
+    componentWillReceiveProps(props) {
+    }
+    componentWillUpdate(props,state) {
+        if(this.props.name!==props.name){
+
+        }
+    }
+    componentDidUpdate(props,state) {
+    }
+
     render() {
         let name = "Nicks";
         return (
