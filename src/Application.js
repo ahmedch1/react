e6b268ef9c08547e2279c511c8711d9198075f3c
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-19 13:50:37 +0100

    component props and re-rendering

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import HighScore from "./HighScore";
 
 class Application extends Component {
     constructor(props) {
@@ -20,16 +21,22 @@ class Application extends Component {
         }
     }
 
+    resetCount = (e) => {
+        console.log("Event is",e);
+        this.setState({
+            count: 0,
+            overTen: false
+        })
+    }
+
     render() {
         let {count} = this.state;
         return (
             <div>
                 <h1>You clicked the button {count} times</h1>
-                {(this.state.overTen) ?
-                    <h3>Beat high score of 10!</h3>
-                    : null
-                }
-
+                <HighScore
+                    overTen={this.state.overTen}
+                    onReset={(e) => this.resetCount(e)}/>
                 <span>
                     <button onClick={() => this.handleClick()}>Click Me</button>
                 </span>
